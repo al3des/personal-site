619ca36de18dadb272a8e702775199696abcd238
al3des <alejandrodesalvo@gmail.com>
2021-02-16 23:00:45 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,68 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import {
+  Container,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core"
+import { getAllPostsWithSlug } from "../lib/graphcms"
+import Intro from "@/components/Intro"
+import Nav from "@/components/Nav"
+import Footer from "@/components/Footer"
+import { theme } from "@/components/themes/default"
 
-export default function Home() {
+let useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    maxHeight: "100vh",
+    display: "grid",
+    gridTemplateAreas: `"header" 
+                        "main"
+                        "footer"`,
+    gridTemplateRows: "auto 1fr auto",
+  },
+  header: {
+    gridArea: "header",
+  },
+  main: {
+    gridArea: "main",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    gridArea: "footer",
+  },
+})
+
+export default function Home({ posts }) {
+  let classes = useStyles()
+
   return (
-    <div className={styles.container}>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <Container className={classes.root}>
+        <CssBaseline />
+        <Nav />
+        <div className={classes.main}>
+          <Intro />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <Footer />
+      </Container>
+    </ThemeProvider>
   )
+}
+
+export async function getStaticProps({ preview = false }) {
+  const posts = (await getAllPostsWithSlug(preview)) || []
+  return {
+    props: { posts, preview },
+  }
 }
