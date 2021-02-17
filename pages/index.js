6d6394a0d75c3ec22d9b0f5b7d144b80c0857249
al3des <alejandrodesalvo@gmail.com>
2021-02-17 16:58:50 +0100

    og meta tags

--- pages/index.js
+++ pages/index.js
@@ -41,12 +41,24 @@ export default function Home({ posts }) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Create Next App</title>
+        <title>Alejandro Desalvo | Frontend Developer</title>
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content="Alejandro Desalvo | Frontend Developer"
+          key="title"
+        />
+        <meta property="og:url" content="https://al3des.dev/" />
+        <meta
+          property="og:image"
+          content="https://avatars.dicebear.com/api/avataaars/6xkW1Zu5k.svg?top[]=shortHair&facialHairChance=70&clothes[]=shirt&clothes[]=hoodie&mouth[]=smile&mouth[]=twinkle&eyes[]=happy&eyes[]=wink&eyes[]=hearts&eyebrow[]=raised&eyebrow[]=uphttps://avatars.dicebear.com/api/avataaars/AGiD4PNWsa.svg?top[]=shortHair&facialHairChance=70&clothes[]=shirt&clothes[]=hoodie&mouth[]=smile&mouth[]=twinkle&eyes[]=happy&eyes[]=wink&eyes[]=hearts&eyebrow[]=raised&eyebrow[]=up"
+        />
+        <meta property="og:type" content="website" />
       </Head>
       <Container className={classes.root}>
         <CssBaseline />
